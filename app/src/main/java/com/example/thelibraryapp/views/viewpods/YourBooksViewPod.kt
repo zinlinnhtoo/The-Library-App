7076ff1a9e912603d734e0b66308082383326b34
zinlinnhtoo <zinlinnhtoo@gmail.com>
2022-11-12 19:09:01 +0630

    setup viewpod in book fragment

---
+++ app/src/main/java/com/example/thelibraryapp/views/viewpods/YourBooksViewPod.kt
@@ -0,0 +1,10 @@
+package com.example.thelibraryapp.views.viewpods
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class YourBooksViewPod @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : RelativeLayout(context, attrs) {
+}
